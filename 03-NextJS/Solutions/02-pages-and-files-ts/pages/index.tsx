a64dacdec46e30e35b6b2b7f14b35747d4bb5ae1
Kevin Cunningham <kevin@kevincunningham.co.uk>
2022-02-09 14:52:32 +0000

    Changes.

--- 03-NextJS/Solutions/02-pages-and-files-ts/pages/index.tsx
+++ 03-NextJS/Solutions/02-pages-and-files-ts/pages/index.tsx
@@ -1,9 +1,8 @@
-import type { NextPage } from 'next'
+import type { NextPage, GetStaticProps } from 'next'
 import { getFeaturedEvents } from "../data/dummy-data"
 import EventList from "../components/events/event-list"
 
-const Home: NextPage = () => {
-  const events = getFeaturedEvents()
+const Home: NextPage<EventProps> = ({ events }) => {
 
   return (
     <div>
@@ -11,5 +10,30 @@ const Home: NextPage = () => {
     </div>
   )
 }
+
+interface Event {
+  id: string,
+  title: string,
+  description: string,
+  location: string,
+  date: string,
+  image: string,
+  isFeatured: boolean,
+}
+
+interface EventProps {
+  events: Event[]
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const events = getFeaturedEvents()
+
+  return {
+    props: {
+      events
+    }
+  }
+}
+
 
 export default Home
